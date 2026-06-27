aa6db6a273304d0a89bedfaee33378408ea32ea1
Saurabh Yadav <saurabhyadav7041916@gmail.com>
2026-06-27 14:10:29 +0530

    connect db

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express"
 import v1Route from "./routes/v1Route.js";
+import dbconnect from "./config/db.js";
 
 
 const app= express();
 
-
+dbconnect();
 app.use("/api/v1",v1Route);
 
 
@@ -12,9 +13,9 @@ app.use("/api/v1",v1Route);
 app.listen(3000,()=>{
     console.log("Server is running")
 })
-=======
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
->>>>>>> 7d18dbcb13e4faadc4005ba6227825655b084ec1
+
