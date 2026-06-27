1d6a203c1894fae3e2c2a40a52448113510e3261
Abhinav Verma <vabhinav228@gmail.com>
2026-06-27 13:47:54 +0530

    added login endpoint and cors middleware

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
