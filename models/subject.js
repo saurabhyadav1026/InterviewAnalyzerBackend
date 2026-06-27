529f278b19f6bd053d5d88ac32997282e6005671
ayush <ayush0ogaz@gmail.com>
2026-06-27 11:41:07 +0530

    subject model added .

---
+++ models/subject.js
@@ -0,0 +1,14 @@
+
+const mongoose = require("mongoose")
+
+const subjectSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+    }
+});
+const Subject = mongoose.model('Subject',subjectSchema);
+
+module.exports=Subject;
