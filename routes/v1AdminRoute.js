ba766d88f9c4ee943872a8adeb3127317b63727b
Saurabh Yadav <saurabhyadav7041916@gmail.com>
2026-06-29 11:51:40 +0530

    bug fix syntax

--- routes/v1AdminRoute.js
+++ routes/v1AdminRoute.js
@@ -1,7 +1,15 @@
 import express from "express";
-import {addQuestion,updateQuestion} from "../controllers/admin-controller/questionController.js"
+import {addQuestion,deleteQues,updateQuestion} from "../controllers/admin-controller/questionController.js"
+import adminMiddleware from "../middlewares/adminMiddleware.js";
+import Subject from "../models/Subject.js";
 
+const AddSub= async () => {
 
+  const sub =await Subject.create({name:"DSA"});
+console.log("subject added")
+  console.log(sub._id)
+return sub;
+}
 
 const v1AdminRoute =express.Router();
 
@@ -10,4 +18,15 @@ v1AdminRoute.post("/question/add",addQuestion);
 v1AdminRoute.post("/question/update/:id",updateQuestion); 
 v1AdminRoute.delete("/question/delete/:id",adminMiddleware,deleteQues)
 
+
+v1AdminRoute.get("/addsub",async(req,res)=>{
+    const resp=await AddSub();
+
+    res.send(resp)
+
+})
+
+
 export default v1AdminRoute;
+
+
