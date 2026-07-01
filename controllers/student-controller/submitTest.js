2849400239844ed48e64fd0388c3c78487f5c851
Saurabh Yadav <saurabhyadav7041916@gmail.com>
2026-07-01 14:08:49 +0530

    change loggin method get to post fix

--- controllers/student-controller/submitTest.js
+++ controllers/student-controller/submitTest.js
@@ -5,52 +5,6 @@ const submitTest=(req,res)=>{
 res.send("test submited")
 }
 
-import mongoose from "mongoose";
-
-const questionSchema = mongoose.Schema({
-
-    question:{
-        type: String,
-        required: true
-    },
-    questionImage:{
-        type:String,
-        default:null
-    },
-
-    options:[{
-        type:String,
-        required: true
-    }],
-
-    answer:{
-        type: String,
-        required: true
-    },
-
-    topic:{
-        type: String,
-        required: String
-    },
-
-    subjectId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Subject'
-    },
-
-    about:{
-        type: String
-    },
-    mark:{
-        type:Number,
-        default:2
-    }
-})
-
-
-const Question = mongoose.model("Question",questionSchema);
-
-export default Question;
 
 
 
